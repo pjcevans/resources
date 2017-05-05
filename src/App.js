a83b7c53d9a4eef6468115de711fdf11d861fcd8
pjcevans <peterjcevans@gmail.com>
2017-05-05 18:15:49 +0100

    added portfolio images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
-import githubLogo from "./images/portfolio/github-logo.png"
-import twitterLogo from "./images/portfolio/twitter-logo.png"
+import githubLogo from "./images/portfolio/githubLogo.png"
+import twitterLogo from "./images/portfolio/twitterLogo.png"
+import screenPreview from "./images/portfolio/screenPreview.png"
+import phonePreview from "./images/portfolio/phonePreview.png"
 
 class App extends Component {
   render() {
     let urls = [
       githubLogo,
-      twitterLogo
+      twitterLogo,
+      phonePreview,
+      screenPreview
     ]
     let images = urls.map((image, index) => <img key={index} src={image} alt={"image number " + index}></img>)
 
