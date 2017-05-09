09d58137485d9b238b72322c33807e7149e462f4
pjcevans <peterjcevans@gmail.com>
2017-05-09 12:05:37 +0100

    tweaked heroku logo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import githubLogo from "./images/portfolio/githubLogo.png"
 import twitterLogo from "./images/portfolio/twitterLogo.png"
 import screenPreview from "./images/portfolio/screenPreview.png"
 import phonePreview from "./images/portfolio/phonePreview.png"
+import herokuLogo from "./images/portfolio/herokuLogo.png"
 
 class App extends Component {
   render() {
@@ -10,7 +11,8 @@ class App extends Component {
       githubLogo,
       twitterLogo,
       phonePreview,
-      screenPreview
+      screenPreview,
+      herokuLogo
     ]
     let images = urls.map((image, index) => <img key={index} src={image} alt={"image number " + index}></img>)
 
