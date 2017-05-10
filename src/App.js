b70e67d3a079e84841493c548c980b9a4bf72dfc
pjcevans <peterjcevans@gmail.com>
2017-05-10 13:18:57 +0100

    added vegan screens

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import twitterLogo from "./images/portfolio/twitterLogo.png"
 import screenPreview from "./images/portfolio/screenPreview.png"
 import phonePreview from "./images/portfolio/phonePreview.png"
 import herokuLogo from "./images/portfolio/herokuLogo.png"
+import veganScreen from "./images/portfolio/veganScreen.png"
+import veganPhone from "./images/portfolio/veganPhone.png"
+
+
 
 class App extends Component {
   render() {
@@ -12,6 +16,8 @@ class App extends Component {
       twitterLogo,
       phonePreview,
       screenPreview,
+      veganPhone,
+      veganScreen,
       herokuLogo
     ]
     let images = urls.map((image, index) => <img key={index} src={image} alt={"image number " + index}></img>)
